50daaa87e80be299ce4b1343dc2d9e984ec49109
James Anwyl <ja.anwyl@gmail.com>
2020-01-17 01:31:52 +0000

    added build folder

---
+++ precache-manifest.5c18022203dd78efdf82c68856bf462d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0bb57330a2e3cd2f0e6c342f84a43b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c12ec0e8153b639de5d",
+    "url": "/static/css/main.c0ed235f.chunk.css"
+  },
+  {
+    "revision": "7e489955e3194b26953a",
+    "url": "/static/js/2.ebc334b0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ebc334b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c12ec0e8153b639de5d",
+    "url": "/static/js/main.4986bb3b.chunk.js"
+  },
+  {
+    "revision": "43bb621dbe45bb4a1411",
+    "url": "/static/js/runtime-main.dab9770b.js"
+  }
+]);
